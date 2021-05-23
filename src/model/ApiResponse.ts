e8f1cd410939ca06b9d30ec3882b3b9f6cd0dc26
dxsn <david.soendoro@kokatto.com>
2021-05-23 09:46:37 +0700

    add multivalueheader response

--- src/model/ApiResponse.ts
+++ src/model/ApiResponse.ts
@@ -2,23 +2,30 @@
  * Response to aAWS Lambda HTTP request event. Used for testing.
  */
 export class ApiResponse {
-    /**
-     * HTTP response headers as a map.
-     */
-    public headers: object
+  /**
+   * HTTP response headers as a map.
+   */
+  public headers: object;
 
-    /**
-     * HTTP response code (201, 400, 500 etc.)
-     */
-    public statusCode: number
+  /**
+   * HTTP response headers with multiple value as a map.
+   */
+  public multiValueHeaders: {
+    [header: string]: (string | number | boolean)[];
+  };
 
-    /**
-     * HTTP response body, potentially Base64 encoded.
-     */
-    public body: string
+  /**
+   * HTTP response code (201, 400, 500 etc.)
+   */
+  public statusCode: number;
 
-    /**
-     * Is the `body` property Base64 encoded?
-     */
-    public isBase64Encoded: boolean
+  /**
+   * HTTP response body, potentially Base64 encoded.
+   */
+  public body: string;
+
+  /**
+   * Is the `body` property Base64 encoded?
+   */
+  public isBase64Encoded: boolean;
 }
